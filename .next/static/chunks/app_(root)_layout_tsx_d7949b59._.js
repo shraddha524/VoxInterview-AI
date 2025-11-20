adbf3c1f5049fdfaf33b5613f9962fac0d8290a6
Shraddha <shraddhamoily392@gmail.com>
2025-11-20 22:14:04 +0530

    Initial commit

---
+++ .next/static/chunks/app_(root)_layout_tsx_d7949b59._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_(root)_layout_tsx_d7949b59._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_(root)_layout_tsx_d7949b59._.js",
+  "chunks": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_01baf433._.js",
+    "static/chunks/node_modules_next_dist_b1877ec8._.js"
+  ],
+  "source": "dynamic"
+});
